f856bdc6dec9e4b804d05711ba511ec27d612c6d
Austin Koehler <truyuri@outlook.com>
2013-11-26 17:07:27 -0600

    Implement Login/Logout UI Behavior
    UI elements hide/show based on login/logout, but login/logout
    authentication procedures are still pending.

--- SAPS/SAPS/SAPS.Designer.cs
+++ SAPS/SAPS/SAPS.Designer.cs
@@ -29,31 +29,43 @@
         private void InitializeComponent()
         {
             this.tabSystems = new System.Windows.Forms.TabControl();
-            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabLogin = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
             this.labelPassword = new System.Windows.Forms.Label();
             this.labelEmail = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.tabApplications = new System.Windows.Forms.TabPage();
             this.btnEdit = new System.Windows.Forms.Button();
             this.buttonSearch = new System.Windows.Forms.Button();
-            this.applicationList = new System.Windows.Forms.ListBox();
             this.tabEvents = new System.Windows.Forms.TabPage();
             this.tabStatistics = new System.Windows.Forms.TabPage();
             this.tabStats = new System.Windows.Forms.TabControl();
             this.tabDefault = new System.Windows.Forms.TabPage();
             this.tabCustom = new System.Windows.Forms.TabPage();
+            this.applicationList = new System.Windows.Forms.ListView();
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.calenderHome = new System.Windows.Forms.MonthCalendar();
+            this.feedNews = new System.Windows.Forms.WebBrowser();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.textWelcome = new System.Windows.Forms.Label();
+            this.textSAPS = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
             this.tabSystems.SuspendLayout();
-            this.tabHome.SuspendLayout();
+            this.tabLogin.SuspendLayout();
             this.tabApplications.SuspendLayout();
             this.tabStatistics.SuspendLayout();
             this.tabStats.SuspendLayout();
+            this.tabHome.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabSystems
             // 
+            this.tabSystems.Controls.Add(this.tabLogin);
             this.tabSystems.Controls.Add(this.tabHome);
             this.tabSystems.Controls.Add(this.tabApplications);
             this.tabSystems.Controls.Add(this.tabEvents);
@@ -65,29 +77,29 @@
             this.tabSystems.Size = new System.Drawing.Size(892, 454);
             this.tabSystems.TabIndex = 0;
             // 
-            // tabHome
+            // tabLogin
             // 
-            this.tabHome.Controls.Add(this.label1);
-            this.tabHome.Controls.Add(this.labelPassword);
-            this.tabHome.Controls.Add(this.labelEmail);
-            this.tabHome.Controls.Add(this.textBox2);
-            this.tabHome.Controls.Add(this.textBox1);
-            this.tabHome.Controls.Add(this.btnLogin);
-            this.tabHome.Location = new System.Drawing.Point(4, 22);
-            this.tabHome.Name = "tabHome";
-            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
-            this.tabHome.Size = new System.Drawing.Size(884, 428);
-            this.tabHome.TabIndex = 0;
-            this.tabHome.Text = "Home";
-            this.tabHome.UseVisualStyleBackColor = true;
+            this.tabLogin.Controls.Add(this.label1);
+            this.tabLogin.Controls.Add(this.labelPassword);
+            this.tabLogin.Controls.Add(this.labelEmail);
+            this.tabLogin.Controls.Add(this.textEmail);
+            this.tabLogin.Controls.Add(this.textPassword);
+            this.tabLogin.Controls.Add(this.btnLogin);
+            this.tabLogin.Location = new System.Drawing.Point(4, 22);
+            this.tabLogin.Name = "tabLogin";
+            this.tabLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLogin.Size = new System.Drawing.Size(884, 428);
+            this.tabLogin.TabIndex = 0;
+            this.tabLogin.Text = "Log In";
+            this.tabLogin.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(308, 106);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(289, 106);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 40);
+            this.label1.Size = new System.Drawing.Size(324, 40);
             this.label1.TabIndex = 5;
             this.label1.Text = "SAPS\r\nStudent Application Processing System";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -110,19 +122,19 @@
             this.labelEmail.TabIndex = 3;
             this.labelEmail.Text = "Email";
             // 
-            // textBox2
+            // textEmail
             // 
-            this.textBox2.Location = new System.Drawing.Point(376, 184);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 20);
-            this.textBox2.TabIndex = 2;
+            this.textEmail.Location = new System.Drawing.Point(376, 184);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(150, 20);
+            this.textEmail.TabIndex = 2;
             // 
-            // textBox1
+            // textPassword
             // 
-            this.textBox1.Location = new System.Drawing.Point(376, 239);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 20);
-            this.textBox1.TabIndex = 1;
+            this.textPassword.Location = new System.Drawing.Point(376, 239);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(150, 20);
+            this.textPassword.TabIndex = 1;
             // 
             // btnLogin
             // 
@@ -136,9 +148,9 @@
             // 
             // tabApplications
             // 
+            this.tabApplications.Controls.Add(this.applicationList);
             this.tabApplications.Controls.Add(this.btnEdit);
             this.tabApplications.Controls.Add(this.buttonSearch);
-            this.tabApplications.Controls.Add(this.applicationList);
             this.tabApplications.Location = new System.Drawing.Point(4, 22);
             this.tabApplications.Name = "tabApplications";
             this.tabApplications.Padding = new System.Windows.Forms.Padding(3);
@@ -164,15 +176,6 @@
             this.buttonSearch.TabIndex = 1;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
-            // 
-            // applicationList
-            // 
-            this.applicationList.FormattingEnabled = true;
-            this.applicationList.Location = new System.Drawing.Point(3, 6);
-            this.applicationList.Name = "applicationList";
-            this.applicationList.Size = new System.Drawing.Size(741, 420);
-            this.applicationList.Sorted = true;
-            this.applicationList.TabIndex = 0;
             // 
             // tabEvents
             // 
@@ -228,6 +231,105 @@
             this.tabCustom.Text = "Custom";
             this.tabCustom.UseVisualStyleBackColor = true;
             // 
+            // applicationList
+            // 
+            this.applicationList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName,
+            this.columnDate});
+            this.applicationList.Location = new System.Drawing.Point(0, 0);
+            this.applicationList.MultiSelect = false;
+            this.applicationList.Name = "applicationList";
+            this.applicationList.Size = new System.Drawing.Size(744, 428);
+            this.applicationList.TabIndex = 3;
+            this.applicationList.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            // 
+            // columnDate
+            // 
+            this.columnDate.Text = "Submission Date";
+            // 
+            // tabHome
+            // 
+            this.tabHome.Controls.Add(this.buttonUpdate);
+            this.tabHome.Controls.Add(this.textName);
+            this.tabHome.Controls.Add(this.textSAPS);
+            this.tabHome.Controls.Add(this.textWelcome);
+            this.tabHome.Controls.Add(this.buttonLogout);
+            this.tabHome.Controls.Add(this.feedNews);
+            this.tabHome.Controls.Add(this.calenderHome);
+            this.tabHome.Location = new System.Drawing.Point(4, 22);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(884, 428);
+            this.tabHome.TabIndex = 4;
+            this.tabHome.Text = "Home";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // calenderHome
+            // 
+            this.calenderHome.Location = new System.Drawing.Point(648, 89);
+            this.calenderHome.Name = "calenderHome";
+            this.calenderHome.TabIndex = 0;
+            // 
+            // feedNews
+            // 
+            this.feedNews.Location = new System.Drawing.Point(-4, 0);
+            this.feedNews.MinimumSize = new System.Drawing.Size(20, 20);
+            this.feedNews.Name = "feedNews";
+            this.feedNews.Size = new System.Drawing.Size(640, 432);
+            this.feedNews.TabIndex = 1;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(683, 400);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(156, 23);
+            this.buttonLogout.TabIndex = 2;
+            this.buttonLogout.Text = "Log Out";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // textWelcome
+            // 
+            this.textWelcome.AutoSize = true;
+            this.textWelcome.Location = new System.Drawing.Point(727, 54);
+            this.textWelcome.Name = "textWelcome";
+            this.textWelcome.Size = new System.Drawing.Size(55, 13);
+            this.textWelcome.TabIndex = 3;
+            this.textWelcome.Text = "Welcome,";
+            // 
+            // textSAPS
+            // 
+            this.textSAPS.AutoSize = true;
+            this.textSAPS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSAPS.Location = new System.Drawing.Point(645, 18);
+            this.textSAPS.Name = "textSAPS";
+            this.textSAPS.Size = new System.Drawing.Size(228, 26);
+            this.textSAPS.TabIndex = 6;
+            this.textSAPS.Text = "SAPS\r\nStudent Application Processing System";
+            this.textSAPS.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textName
+            // 
+            this.textName.AutoSize = true;
+            this.textName.Location = new System.Drawing.Point(726, 67);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(56, 13);
+            this.textName.TabIndex = 7;
+            this.textName.Text = "user name";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(683, 371);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(156, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
             // winSAPS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -237,11 +339,13 @@
             this.Name = "winSAPS";
             this.Text = "Student Application Processing System";
             this.tabSystems.ResumeLayout(false);
-            this.tabHome.ResumeLayout(false);
-            this.tabHome.PerformLayout();
+            this.tabLogin.ResumeLayout(false);
+            this.tabLogin.PerformLayout();
             this.tabApplications.ResumeLayout(false);
             this.tabStatistics.ResumeLayout(false);
             this.tabStats.ResumeLayout(false);
+            this.tabHome.ResumeLayout(false);
+            this.tabHome.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -255,16 +359,26 @@
         private System.Windows.Forms.TabControl tabStats;
         private System.Windows.Forms.TabPage tabDefault;
         private System.Windows.Forms.TabPage tabCustom;
-        private System.Windows.Forms.ListBox applicationList;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button buttonSearch;
-        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabLogin;
         private System.Windows.Forms.Label labelPassword;
         private System.Windows.Forms.Label labelEmail;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPassword;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView applicationList;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnDate;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label textName;
+        private System.Windows.Forms.Label textSAPS;
+        private System.Windows.Forms.Label textWelcome;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.WebBrowser feedNews;
+        private System.Windows.Forms.MonthCalendar calenderHome;
     }
 }
 
--- SAPS/SAPS/SAPS.cs
+++ SAPS/SAPS/SAPS.cs
@@ -20,11 +20,39 @@ namespace SAPS
             _baseSystem = new BaseSystem();
 
             InitializeComponent();
+
+            this.tabSystems.SelectedTab = tabLogin;
+            this.tabSystems.Controls.Remove(tabHome);
+            this.tabSystems.Controls.Remove(tabApplications);
+            this.tabSystems.Controls.Remove(tabEvents);
+            this.tabSystems.Controls.Remove(tabStatistics);
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            // do error checking here first
 
+            // if successful
+            this.tabSystems.Controls.Add(tabHome);
+            this.tabSystems.SelectedTab = tabHome;
+            this.tabSystems.Controls.Remove(tabLogin);
+            this.tabSystems.Controls.Add(tabApplications);
+            this.tabSystems.Controls.Add(tabEvents);
+            this.tabSystems.Controls.Add(tabStatistics);
+
+            // load database
+            // load applications and events
+            // calculate basic stats
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            this.tabSystems.Controls.Add(tabLogin);
+            this.tabSystems.SelectedTab = tabLogin;
+            this.tabSystems.Controls.Remove(tabHome);       
+            this.tabSystems.Controls.Remove(tabApplications);
+            this.tabSystems.Controls.Remove(tabEvents);
+            this.tabSystems.Controls.Remove(tabStatistics);
         }
     }
 }
